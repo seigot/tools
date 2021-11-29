5c1d27514dc78306f0743d28a13df80cb639a524
seigot <46782860+seigot@users.noreply.github.com>
2021-11-30 01:57:13 +0900

    Add files via upload

---
+++ robot/test.sh
@@ -0,0 +1,56 @@
+
+CATKIN_WS_SRC=${HOME}/catkin_ws/src
+
+# sample
+function common(){
+    REPOSITORY_URL=$1
+    if [ -z "${REPOSITORY_URL}" ];then
+	# sample
+	REPOSITORY_URL="https://github.com/seigot/burger_war_dev"
+    fi
+
+    cd $HOME/catkin_ws/src
+    sudo rm -r burger_war_dev
+    git clone ${REPOSITORY_URL}
+
+    # catkin build
+    cd $HOME/catkin_ws
+    catkin build
+    source $HOME/catkin_ws/devel/setup.bash
+
+    # autotest
+    cd $HOME/catkin_ws/src/burger_war_kit
+    bash autotest/autotest.sh 
+}
+
+# takino-san
+function takinoon(){
+    common "https://github.com/takinoon/burger_war_dev"
+}
+
+# amamiya-san
+function nanka-nemuiyo(){
+    common "https://github.com/nanka-nemuiyo/burger_war_dev"
+}
+
+# ohishi-san
+function KoutaOhishi(){
+    common "https://github.com/KoutaOhishi/burger_war_dev -b develop"    
+}
+
+# onishi-san
+function k-onishi(){
+    common "https://github.com/k-onishi/burger_war_dev -b develop"
+}
+
+# tanimura-san
+function ce31062(){
+    common "https://github.com/ce31062/burger_war_dev -b develop"
+}
+
+#common
+takinoon
+#nanka-nemuiyo
+#KoutaOhishi
+#k-onishi
+#ce31062
