30f63b112a61c7c6732cbddde14b1625549b83d4
seigot <s.takada.3o3@gmail.com>
2023-09-05 00:40:44 +0900

    update

--- tetris/art/artlib_search32_xsxs.py
+++ tetris/art/artlib_search32_xsxs.py
@@ -4,7 +4,7 @@ import copy
 import numpy
 import time
 
-
+# 現在の盤面を表す2次元配列
 board = [
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
@@ -15,9 +15,9 @@ board = [
         [0,0,0,0,0,0,0,0,0,0],
         [9,9,9,9,9,9,9,9,9,9],
         ]
-
+# boardの高さ方向のrange(1番下の9999999999はoffsetなので高さ方向にカウントしない)
 range_y = len(board)-1
-
+# 各shapeの回転の基準点以外のブロックの相対座標(shape1~7まで存在する)
 refpoint_relative = [[0],
                     [[[0,-1],[0,1],[0,2]],[[-2,0],[-1,0],[1,0]]],
                     [[[0,-1],[0,1],[1,1]],[[-1,0],[1,0],[-1,1]],[[-1,-1],[0,-1],[0,1]],[[1,-1],[-1,0],[1,0]]],
@@ -27,7 +27,7 @@ refpoint_relative = [[0],
                     [[[0,-1],[1,-1],[-1,0]],[[0,-1],[1,0],[1,1]]],
                     [[[-1,-1],[0,-1],[1,0]],[[1,-1],[1,0],[0,1]]]
                     ]
-
+# 各shapeのx軸方向の探索範囲(1~7)
 Xrange = [[0],
         [[0,10],[2,9]],
         [[0,9],[1,9],[1,10],[1,9]],
@@ -37,17 +37,26 @@ Xrange = [[0],
         [[1,9],[0,9]],
         [[1,9],[0,9]]
             ]
-
+# ブロックの組み合わせとboardの探索盤面, 最大8こ分
 status = [[[0,0,0,1],[0,0,0,1],[0,0,0,1],[0,0,0,1],[0,0,0,1],[0,0,0,1],[0,0,0,1],[0,0,0,1]],
 board,  board,  board,  board,  board,  board, board,  board,  board ]
 
+# ブロックの組み合わせの並び
+# 見つかりやすい順番に並べている (6,7は最も組み合わせを見つけやすい、1,5は最も組み合わせを見つけにくい)
 blockorder = [6,7,4,2,3,1,5]
 
-
+# xs_xsの組み合わせをDFS(深さ優先探索)により探索する
+# input
+#   currentboard:
+#   num_move:
+#   status:
+# output
+#   ans: 組み合わせのブロック
 def dfs(currentboard, num_move, status):
     global foundflag
     num_move += 1
     if num_move > maxmoves:
+        # 期待通りの盤面かどうかチェック
         # final check of the board
         # two x-coords must have the two shape indexes respectively. Others must be empty         
         printflag1 = CheckTwoBlocks(currentboard = status[maxmoves])
@@ -76,8 +85,12 @@ def dfs(currentboard, num_move, status):
                         grid1 = newboard[refpoint[1] + refpoint_relative[shape][rotate][1][1]][refpoint[0] +  refpoint_relative[shape][rotate][1][0]]
                         grid2 = newboard[refpoint[1] + refpoint_relative[shape][rotate][2][1]][refpoint[0] +  refpoint_relative[shape][rotate][2][0]]
                         if gridref == 0 and grid0 == 0 and grid1 == 0 and grid2 == 0:
+                            # 1つでも0でない(既存の盤面と重なりが生じる)部分を見つけたいと思っており、
+                            # それに該当しない場合はスキップ
                             pass
                         else:
+                            # 1つでも0でない部分を見つけたら、その1つ上に対象のミノを置けるようにする
+                            # 具体的にはここでrefpointを更新する(Ygrid=Ygrid-1に更新する)
                             refpoint = [refpoint[0],refpoint[1]-1]
 
                             # simulate the board after the block is dropped
@@ -87,11 +100,11 @@ def dfs(currentboard, num_move, status):
 
                             # clear fulllines
                             clearedboard = clearFullLines(currentboard = newboard)
-
+                            # 各種チェック(後述)
                             contflag1 = CheckBottomRow(currentboard = clearedboard)
                             contflag2 = CheckTopRow(currentboard = clearedboard)
                             contflag3 = CheckHoles(currentboard = clearedboard)
-
+                            # チェックが全てOKであれば探索対象として妥当な盤面なので次の探索に移る
                             if contflag1 == True and contflag2== True and contflag3 == True:
                                 status[0][num_move-1][0] = shape
                                 status[0][num_move-1][1] = rotate
@@ -103,13 +116,18 @@ def dfs(currentboard, num_move, status):
 
                                 ans = dfs(currentboard = status[num_move],num_move = num_move,status = status)
                                 if foundflag == True:
+                                    # 見つかったらansを返す
                                     return ans
+                                # 見つからなかったらstatusを元に戻す
                                 status[0][num_move-1] = [0,0,0,1]
                                     
                             else:
                                 break
                             break
 
+# 一番下の行を確認し
+# 探索対象のブロック(x1,shape1),(x2,shape2)以外のブロックがないことをチェックする
+# 探索対象ではないブロックが1つでも存在していたらFalse
 def CheckBottomRow(currentboard):
     BottomRow = currentboard[range_y - 1]
     i = 0
@@ -123,6 +141,10 @@ def CheckBottomRow(currentboard):
     return True
 
 
+# 一番下の行を確認し、
+# 探索結果が探索対象の組み合わせ(x1shape1_x2shape2)であるかどうかをチェックする
+# 探索対象の組み合わせである: True
+# そうでない: False
 def CheckTwoBlocks(currentboard):
     board = currentboard
     Y = range_y -1 
@@ -142,6 +164,8 @@ def CheckTwoBlocks(currentboard):
                 return False
     return True
 
+# 4*8=32の探索を前提としているのでそれでブロックを消せない場合は探索対象外とする
+# 具体的にはboardの上3行にブロックが1つでも存在していたらFalseとする
 def CheckTopRow(currentboard):
     for num in range(3):
         TopRow = currentboard[num]
@@ -150,17 +174,22 @@ def CheckTopRow(currentboard):
                 return False
     return True
 
+# 1行揃ったら対象行を削除する
 def clearFullLines(currentboard):
     board = currentboard
     for i in range(range_y):
         row = board[i]
         if row.count(0) == 0:
             for j in range(i,0,-1):
+                # ここで削除対象行を基準にboardを一段ずらす
                 board[j] = board[j-1]
             board[0] = [0,0,0,0,0,0,0,0,0,0]
             
     return board
 
+# Holeが存在しているかどうかチェック
+# boardの下から上の順に操作してHoleが存在していたらFalse
+# Holeが存在していなければTrue
 def CheckHoles(currentboard):
     board = currentboard
     for X in range(10):
@@ -174,6 +203,7 @@ def CheckHoles(currentboard):
 
     return True
 
+# 一番下の行を除いて全て削除できているかを確認する
 def CheckClearRows(currentboard):
     for num in range(range_y - 1):
         TopRow = currentboard[num]
@@ -182,7 +212,7 @@ def CheckClearRows(currentboard):
                 return False
     return True
 
-
+# xs_xsの組み合わせを探索する
 def search_artlib():
     global foundflag
     foundflag = False
@@ -193,11 +223,16 @@ def search_artlib():
         return False
     return ans
 
+# 該当の組み合わせの探索結果をログファイルに出力する
+# ans
+# True: 見つかった(見つかった場合は組み合わせをリスト形式で出力する)
+# False: 見つからない
 def write_in_txt(ans):
     path = '..\\tetris\\game_manager\\log_art.txt'
     with open (path, mode ="a") as f:
         f.write('"xs_' + str(x1)+str(shape1)+str(x2)+str(shape2)+ '":' + str(ans)+ "," + "\n")
 
+# ミノ1つ4ブロック*8 = 32(3行+2つ)の範囲で組み合わせを探索する
 maxmoves = 8
 # for each combination of two x-coords and two shapes
 for x1 in range(10):
